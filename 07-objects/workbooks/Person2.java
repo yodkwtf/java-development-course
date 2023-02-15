39339b3ccbc69c77e123c1b9793001293b162fa4
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-02-16 01:21:06 +0530

    constructor workbook

---
+++ 07-objects/workbooks/Person2.java
@@ -0,0 +1,13 @@
+public class Person2 {
+  String name;
+  String nationality;
+  String dateOfBirth;
+  int seatNumber;
+
+  public Person2(String name, String nationality, String dateOfBirth, int seatNumber) {
+    this.name = name;
+    this.nationality = nationality;
+    this.dateOfBirth = dateOfBirth;
+    this.seatNumber = seatNumber;
+  }
+}
---
+++ 07-objects/workbooks/Workbook2.java
@@ -0,0 +1,8 @@
+public class Workbook2 {
+  public static void main(String[] args) {
+    Person2 p = new Person2("Durgesh", "Indian", "04/08/2001", 12456);
+
+    System.out.println("Name: " + p.name + "\n" + "Nationality: " + p.nationality + "\n" + "Date of Birth: "
+        + p.dateOfBirth + "\n" + "Seat Number: " + p.seatNumber + "\n");
+  }
+}
