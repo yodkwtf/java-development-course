69f0265db938728a2c1222d337e1cabd1a1f0ed4
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-02-17 03:17:25 +0530

    workbook 6

---
+++ 07-objects/workbooks/Workbook6.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Workbook6 {
+  public static void main(String[] args) {
+    Person6 person = new Person6("Rayan Slim", "Canadian", "01/01/1111", 5);
+
+    System.out.println(
+        "Name: " + person.getName() + "\n" + "Nationality: " + person.getNationality() + "\n" + "Date of Birth: "
+            + person.getDateOfBirth() + "\n" + "Seat Number: " + person.getSeatNumber() + "\n" + "Passport: " +
+            Arrays.toString(person.getPassport()) + "\n");
+
+    // Passport application
+    if (person.applyPassport()) {
+      System.out.println("Congratulations " + person.getName() + ". Your passport was approved!\n");
+      person.setPassport();
+    } else {
+      System.out.println("We are sorry " + person.getName() + ". We cannot process your application.\n");
+    }
+
+    System.out.println(
+        "Name: " + person.getName() + "\n" + "Nationality: " + person.getNationality() + "\n" + "Date of Birth: "
+            + person.getDateOfBirth() + "\n" + "Seat Number: " + person.getSeatNumber() + "\n" + "Passport: " +
+            Arrays.toString(person.getPassport()) + "\n");
+  }
+}
