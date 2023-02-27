6d9ce4354a8124bc162a5971f755117d734502c4
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-02-28 02:17:35 +0530

    hashmap fixes

--- 10-map-collections/HashMapExample.java
+++ 10-map-collections/HashMapExample.java
@@ -1,9 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class HashMapInventory {
+public class HashMapExample {
   public static void main(String[] args) {
-
     // Create a HashMap object called inventory
     Map<String, Double> inventory = new HashMap<>();
     inventory.put("Bananas", 2.99);
@@ -19,18 +18,18 @@ public class HashMapInventory {
     System.out.println("\nWelcome to Java Grocers. What can we help you find?:\n");
 
     System.out.println("\nWhat is the price of Lime?");
-    System.out.println(inventory.get("Lime"));
+    System.out.println(inventory.get("Lime")); // 0.49
 
     System.out.println("\nWhat is the price of Zucchini?");
-    System.out.println(inventory.get("Zucchini"));
+    System.out.println(inventory.get("Zucchini")); // 5.99
 
     System.out.println("\nWhat is the price of Kale?");
-    System.out.println(inventory.get("Kale"));
+    System.out.println(inventory.get("Kale")); // 4.99
 
     System.out.println("\nDo you guys sell Apples?");
     boolean result = inventory.containsKey("Apples");
     String response = result ? "Yes!" : "No, sorry!";
-    System.out.println(response);
+    System.out.println(response); // Yes!
 
   }
 }
