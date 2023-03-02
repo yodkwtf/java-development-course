afca7ce3f9e49932583ec659b0ad731517b11bce
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-03-02 23:51:49 +0530

    inheritance workbook

---
+++ 11-inheritance/workbooks/one/Main.java
@@ -0,0 +1,17 @@
+import shape.Cylinder;
+import shape.Sphere;
+
+public class Main {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder();
+        cylinder.setHeight(2.0);
+        cylinder.setRadius(11.0);
+
+        Sphere sphere = new Sphere();
+        sphere.setRadius(1.0);
+
+        System.out.println("Sphere - radius: " + sphere.getRadius());
+        System.out.println("Cylinder - radius: " + cylinder.getRadius() + " height " + cylinder.getHeight());
+    }
+
+}
---
+++ 11-inheritance/workbooks/one/shape/Cylinder.java
@@ -0,0 +1,13 @@
+package shape;
+
+public class Cylinder extends Shape {
+    private double height;
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
